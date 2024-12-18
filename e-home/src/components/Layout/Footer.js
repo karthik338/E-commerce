064dde28eb00a7b4ca4f68089291493fbbac6b99
karthik338 <ramkarthikvoolla@gmail.com>
2024-12-18 18:20:55 +0530

    commit

--- e-home/src/components/Layout/Footer.js
+++ e-home/src/components/Layout/Footer.js
@@ -23,12 +23,12 @@ const Footer = () => {
 
     return (
         <Container fluid>
-      <div className="copyright">&copy; 2024 e-Home. All Right Reserved.</div>
+      <div className="copyright">&copy; 2024 e-Shop. All Right Reserved.</div>
       <div className="socials">
         <ul>
-          <li><a href="https://www.facebook.com"><i className="fab fa-facebook-f"></i></a></li>
+          <li><a href="https://www.facebook.com"><i class="fab fa-facebook"></i></a></li>
           <li><a href="https://www.twitter.com"><i className="fab fa-twitter"></i></a></li>
-          <li><a href="https://www.linkedin.com"><i className="fab fa-linkedin-in"></i></a></li>
+          <li><a href="https://www.linkedin.com"><i className="fab fa-linkedin"></i></a></li>
         </ul>
       </div>
       {
